9aedba3efd7a47b0465152ae2f4687460156cd57
Pikachu-pika-pika <67504057+Pikachu-pika-pika@users.noreply.github.com>
2020-06-27 18:54:41 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -10,8 +10,8 @@ pipeline {
                                       usernameVariable: 'username',
                                       passwordVariable: 'password')])
                        { try 
-                            { sh "echo '$(password)' | sudo -S docker stop isng"
-                              sh "echo '$(password)' | sudo -S docker container isng"
+                            { sh "echo '$(password)' | sudo -S docker stop AnnaMerzliakova"
+                              sh "echo '$(password)' | sudo -S docker container rm AnnaM"
                          catch (Exception e) {
                             print 'container not exist, skip clean'}
                          }
@@ -38,8 +38,8 @@ pipeline {
                                       usernameVariable: 'username',
                                       passwordVariable: 'password')])
                   {
-                    sh "echo '$(password)' | sudo -S docker build ${WORKSPACE}/auto -t Anna_Merzliakova_nginx"
-                    sh "echo '$(password)' | sudo -S docker run -d -p 6784:80 --name AnnaMerzliakovanginx -v /home/adminci/is_mount_dir:/start Anna_Merzliakova_nginx
+                    sh "echo '$(password)' | sudo -S docker build ${WORKSPACE}/auto -t AnnaM"
+                    sh "echo '$(password)' | sudo -S docker run -d -p 6784:80 --name AnnaM -v /home/adminci/is_mount_dir:/start AnnaM
                 }
             }
         }
@@ -50,8 +50,8 @@ pipeline {
                     withCredentials([ usernamePassword(credentialsId: 'srv_sudo',
                                       usernameVariable: 'username',
                                       passwordVariable: 'password')])
-                  { sh "echo '$(password)' | sudo -S docker exec -t AnnaMerzliakovanginx bash -c 'df -h > /start/states.txt
-                    sh "echo '$(password)' | sudo -S docker exec -t AnnaMerzliakovanginx bash -c 'top -n >> /start/states.txt
+                  { sh "echo '$(password)' | sudo -S docker exec -t AnnaM bash -c 'df -h > /start/states.txt
+                    sh "echo '$(password)' | sudo -S docker exec -t AnnaM bash -c 'top -n >> /start/states.txt
                 }
             }
         }
