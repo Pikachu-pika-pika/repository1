f2834c1aaa8072ca324983731de13df20f0abb90
Pikachu-pika-pika <67504057+Pikachu-pika-pika@users.noreply.github.com>
2020-06-29 18:13:49 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -35,7 +35,7 @@ pipeline {
                 script{withCredentials([ usernamePassword(credentialsId: 'srv_sudo',
                                           usernameVariable: 'username',
                                           passwordVariable: 'password')])
-                  {sh "echo '${password}' | sudo -S docker run -d -p 6784:80 --name anna_m -v /home/adminci/is_mount_dir:/start AnnaM"}
+                  {sh "echo '${password}' | sudo -S docker run -d -p 6784:80 --name anna_m -v /home/adminci/is_mount_dir:/start anna_m"}
                 }
             }
         }
